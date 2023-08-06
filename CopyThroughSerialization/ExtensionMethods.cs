d385e284c1738d20f1ab1d568dd68a1497274613
David <David@DESKTOP-G2>
2023-08-06 11:03:53 +0200

    Copy through serialization added

---
+++ CopyThroughSerialization/ExtensionMethods.cs
@@ -0,0 +1,36 @@
+﻿
+using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+namespace CopyThroughSerialization
+{
+    public static class ExtenionMethods
+    {
+        //public static T DeepCopy<T>(this T self) //extension Methode on any type
+        //{
+        //    MemoryStream stream = new MemoryStream();
+        //    BinaryFormatter formatter = new BinaryFormatter();
+        //    formatter.Serialize(stream, self);
+        //    stream.Seek(0, SeekOrigin.Begin);
+        //    object copy = formatter.Deserialize(stream);
+        //    stream.Close();
+        //    return (T)copy;
+        //}
+
+        public static T? DeepCopyXml<T>(this T self)
+        {
+            using var stream = new MemoryStream();
+            var s = new XmlSerializer(typeof(T));
+            s.Serialize(stream, self);
+            stream.Position = 0;
+            return (T)s.Deserialize(stream);
+        }
+
+        public static T? DeepCopyJson<T>(this T self)
+        {
+            if (ReferenceEquals(self, null)) return default;
+            var deserializeSettings = new JsonSerializerSettings { ObjectCreationHandling = ObjectCreationHandling.Replace };
+            return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(self), deserializeSettings);
+        }
+    }
+}
---
+++ CopyThroughSerialization/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CopyThroughSerialization
+{
+    internal class Program
+    {
+
+
+        [Serializable]
+        public class Person 
+        {
+            public string[] Names;
+            public Address Addresse;
+
+            public Person()
+            {
+                    
+            }
+
+            public Person(string[] name, Address address)
+            {
+                Names = name;
+                Addresse = address;
+            }
+
+            //Copy construktor
+            public Person(Person copy)
+            {
+                Names = copy.Names;
+                Addresse = new Address(copy.Addresse);
+            }
+
+            public override string ToString()
+            {
+                return $"{nameof(Names)}: {string.Join(" ", Names)},{nameof(Addresse)}: {Addresse} ";
+            }
+
+ 
+        }
+
+
+        [Serializable]
+        public class Address
+        {
+            public string StreetName;
+            public int HouseNumber;
+
+            public Address()
+            {
+
+            }
+
+            public Address(string streetName, int houseNumber)
+            {
+                StreetName = streetName;
+                HouseNumber = houseNumber;
+            }
+
+
+            //copy construktor
+            public Address(Address copy)
+            {
+                StreetName = copy.StreetName;
+                HouseNumber = copy.HouseNumber;
+            }
+
+            public override string ToString()
+            {
+                return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            var john = new Person(new string[] { "John", "Smith" }, new Address("Kehlegg", 123));
+
+
+            Person david = john.DeepCopyJson();
+
+            david.Names = new string[] { "David", "Schmoranz" };
+            david.Addresse.StreetName = "Nenzing";
+            david.Addresse.HouseNumber = 321;
+
+            Console.WriteLine(john);
+            Console.WriteLine(david);
+        }
+    }
+}
