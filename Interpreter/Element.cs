679f1e66a3123f8c50e67b53f2947aa613b95ff5
David Schmoranz <david.schmoranz@outlook.com>
2024-05-03 22:05:45 +0200

    Added Interpreter

---
+++ Interpreter/Element.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace Interpreter;
+
+public interface IElement
+{
+    int Value { get; }
+
+}
+public class Integer : IElement
+{
+    public Integer(int value)
+    {
+        Value = value;
+    }
+
+    public int Value { get; }
+}
+
+public class BinaryOperation : IElement
+{
+    public int Value
+    {
+        get
+        {
+            switch (MyType)
+            {
+                case Type.Addition:
+                    return Left.Value + Right.Value;
+                case Type.Subraction:
+                    return Left.Value - Right.Value;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+
+    public enum Type
+    {
+        Addition, Subraction
+    }
+
+    public Type MyType;
+    public IElement Left, Right;
+
+}
