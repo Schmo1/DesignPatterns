518601cae300c936f2c5607849d85c4ea9842ad0
Schmo1 <93264962+Schmo1@users.noreply.github.com>
2025-05-21 22:12:14 +0200

    Proxy

---
+++ ProtectionProxy/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ProtectionProxy;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		ICar car = new CarProxy(new Driver { Age = 18 });
+	}
+}
+
+public interface ICar
+{
+	void Drive();
+}
+public class  Car
+{
+	public void Drive()
+	{
+		Console.WriteLine("Car is being driven");
+	}
+}
+
+
+public class CarProxy : ICar
+{
+	private readonly Driver driver;
+	private readonly Car car = new Car();
+
+	public CarProxy(Driver driver)
+	{
+	
+		this.driver = driver;
+	}
+	public void Drive()
+	{
+		if (driver.Age > 17)
+		{
+			car.Drive();
+		}
+		else
+		{
+			Console.WriteLine("Access denied");
+		}
+	}
+}
+
+public class Driver()
+{
+	public int Age { get; set; }
+}
---
+++ StringFormatter/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Text;
+
+namespace StringFormatter
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var formattedString = new FormattedString("Hello World!");
+			formattedString.Capitalize(7, 11);
+			Console.WriteLine(formattedString.ToString());
+
+			var betterFormattedString = new BetterFormattedString("Hello World!");
+			betterFormattedString.GetRange(7, 11).Capitalized = true;
+			Console.WriteLine(betterFormattedString.ToString());
+		}
+
+		public class FormattedString
+		{
+			private readonly string text;
+			private readonly bool[] capitalized;
+
+			public FormattedString(string input)
+			{
+				this.text = input;
+				capitalized = new bool[input.Length];
+			}
+
+			public void Capitalize(int start, int end)
+			{
+				for (int i = start; i < end; i++)
+				{
+					capitalized[i] = true;
+				}
+			}
+
+			public override string ToString()
+			{
+				var sb = new StringBuilder();
+
+				for (int i = 0; i < text.Length; i++)
+				{
+					var c = text[i];
+					sb.Append(capitalized[i] ? char.ToUpper(c) : c);
+				}
+				return sb.ToString();
+			}
+		}
+
+		public class BetterFormattedString
+		{
+			private readonly string text;
+			private readonly List<TextRange> formatting = [];
+
+			public BetterFormattedString(string input)
+			{
+				this.text = input;
+			}
+
+			public TextRange GetRange(int start, int end)
+			{
+				var range = new TextRange
+				{
+					Start = start,
+					End = end
+				};
+				formatting.Add(range);
+				return range;
+
+			}
+
+			public override string ToString()
+			{
+				var sb = new StringBuilder();
+				for (int i = 0; i < text.Length; i++)
+				{
+					var c = text[i];
+					foreach (var range in formatting)
+					{
+						if (range.Covers(i))
+						{
+							if (range.Capitalized) c = char.ToUpper(c);
+						}
+					}
+					sb.Append(c);
+
+				}
+				return sb.ToString();
+			}
+		}
+
+		public class TextRange
+		{
+			public int Start, End;
+			public bool Capitalized, Bold, Italic;
+			public bool Covers(int position)
+			{
+				return position >= Start && position <= End;
+			}
+		}
+	}
+}
+		
