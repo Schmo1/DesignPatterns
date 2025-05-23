acf79455257f5200f8b730f24260c949e7987686
David Schmoranz <david.schmoranz@outlook.com>
2025-05-23 15:37:34 +0200

    Proxy added

---
+++ PropertyProxy/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace PropertyProxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+
+    public class Creature
+    {
+        private Property<int> agility = new();
+        public int Agility
+        {
+            get { return agility.Value; }
+            set { agility.Value = value; }
+        }
+
+        public class Property<T> : IEquatable<Property<T>> where T : new()
+        {
+            private T _value;
+            public T Value
+            {
+                get => _value;
+                set
+                {
+                    if (Equals(_value)) return;
+
+                    Console.WriteLine($"Assigning value to {value}");
+                    _value = value;
+                }
+            }
+
+            public Property() : this(Activator.CreateInstance<T>())
+            {
+
+            }
+
+
+            public Property(T value)
+            {
+                _value = value;
+            }
+
+            public static implicit operator T(Property<T> property) => property.Value;
+
+            public static implicit operator Property<T>(T value) => new Property<T>(value);
+
+            public override int GetHashCode()
+            {
+                return Value.GetHashCode();
+            }
+
+            public bool Equals(Property<T>? other)
+            {
+                if (ReferenceEquals(null, other)) return false;
+                if (ReferenceEquals(this, other)) return true;
+                return EqualityComparer<T>.Default.Equals(Value, other);
+            }
+
+            public static bool operator ==(Property<T> left, Property<T> right)
+            {
+                return Equals(left, right);
+            }
+        }
+    }
+
+}
---
+++ ValueProxy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+namespace ValueProxy
+{
+    public struct Percentage
+    {
+        [DebuggerDisplay("{value*100.0f}%")]
+        private readonly float value;
+
+        internal Percentage(float value)
+        {
+            this.value = value;
+        }
+
+        public static float operator *(float f, Percentage p)
+        {
+            return f * p.value;
+        }
+
+        public static Percentage operator +(Percentage p1, Percentage p2)
+        {
+            return new Percentage(p1.value + p2.value);
+        }
+
+        public override string ToString()
+        {
+            return $"{value * 100}%";
+        }
+
+
+    }
+
+    public static class PercentageExtensions
+    {
+        public static Percentage Percent(this float value)
+        {
+            return new Percentage(value / 100.0f);
+        }
+
+        public static Percentage Percent(this int value)
+        {
+            return new Percentage(value / 100.0f);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(10f * 5.Percent());
+            Console.WriteLine(2.Percent() + 5.Percent());
+        }
+    }
+}
