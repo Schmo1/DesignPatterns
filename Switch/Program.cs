538135bdc0b4e3c20827a6e1f378fc1cbe1b401e
Schmo1 <93264962+Schmo1@users.noreply.github.com>
2025-12-10 21:34:32 +0100

    Switch State

---
+++ Switch/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Switch
+{
+	internal class Program
+	{
+		enum State
+		{
+			Locked,
+			Failed,
+			Unlocked
+		}
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				State state = State.Locked;
+				Console.Write("Enter password: ");
+				string? password = Console.ReadLine();
+				switch (state)
+				{
+					case State.Locked when password == "secret":
+						state = State.Unlocked;
+						goto case State.Unlocked;
+						
+					case State.Locked:
+						state = State.Failed;
+						break;
+					case State.Failed:
+						Console.WriteLine("Alarm! Incorrect password.");
+						state = State.Locked;
+						break;
+					case State.Unlocked:
+						Console.WriteLine("Door unlocked. Welcome!");
+						state = State.Locked;
+						break;
+				}
+			}
+		}
+	}
+}
