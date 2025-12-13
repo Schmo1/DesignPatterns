2852695b2526a31be2fe12a138595501345ad52c
Schmo1 <93264962+Schmo1@users.noreply.github.com>
2025-12-13 13:48:30 +0100

    Zwei neue Projekte: Equality & TemplateMethod hinzugefügt
    EqualityAndComparisonStrategies und TemplateMethod als neue .NET 10.0 Konsolenprojekte zur Solution hinzugefügt. EqualityAndComparisonStrategies demonstriert Vergleichs- und Gleichheitsstrategien mit einer Person-Klasse. TemplateMethod zeigt das Template Method Pattern anhand eines Schachspiels. Solution- und Projektdateien entsprechend erweitert.

---
+++ EqualityAndComparisonStrategies/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace EqualityAndComparisonStrategies
+{
+	class Person : IComparable<Person>, IComparable
+	{
+		public int Age;
+		public int Id;
+		public string Name;
+
+		public Person(int age, int id, string name)
+		{
+			Age = age;
+			Id = id;
+			Name = name;			
+		}
+
+		public int CompareTo(Person? other)
+		{
+			if(ReferenceEquals(this, other)) return 0;
+			if(ReferenceEquals(null, other)) return 1;
+			return Id.CompareTo(other.Id);
+		}
+
+		public int CompareTo(object? obj)
+		{
+			if (ReferenceEquals(null, obj)) return 1;
+			if (ReferenceEquals(this, obj)) return 0;
+			return obj is Person other ? CompareTo(other) : throw new ArgumentException();
+		}
+
+		private sealed class NameRelationalComparer : IComparer<Person>
+		{
+			public int Compare(Person? x, Person? y)
+			{
+				if(ReferenceEquals (x, y)) return 0;
+				if (ReferenceEquals(null, y)) return 1;
+				if (ReferenceEquals(null, x)) return -1;
+				return string.Compare (x.Name, y.Name, StringComparison.OrdinalIgnoreCase);
+			}
+		}
+
+		public static IComparer<Person> NameComparer { get; } = new NameRelationalComparer();
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var people = new List<Person>();
+
+			people.Sort(); // default
+
+			people.Sort((x,y) => x.Name.CompareTo(y.Name));
+
+			people.Sort(Person.NameComparer);
+		}
+	}
+}
---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace TemplateMethod
+{
+
+	public abstract class Game
+	{
+		protected abstract bool HaveWinner { get; }
+		protected abstract int WinningPlayer { get; }
+		public void Run()
+		{
+			Start();
+			while (!HaveWinner)
+			{
+				TakeTurn();
+			}
+
+			Console.WriteLine($"Player {WinningPlayer} winns.");
+		}
+
+		protected abstract void Start();
+		protected abstract void TakeTurn();
+
+
+		protected int currentPlayer;
+		protected readonly int numberOfPlayers;
+
+		protected Game(int numberOfPlayers) 
+		{
+			this.numberOfPlayers = numberOfPlayers;
+		}
+
+	}
+
+	public class Chess : Game
+	{
+		protected override bool HaveWinner => turn == maxTurns;
+
+		protected override int WinningPlayer => currentPlayer;
+
+		public Chess() : base(2)
+		{
+			
+		}
+
+		protected override void Start()
+		{
+			Console.WriteLine($"Starting chess game. Number of Players {numberOfPlayers}");
+		}
+
+		protected override void TakeTurn()
+		{
+			Console.WriteLine($"Turn {turn++} taken by player {currentPlayer}");
+			currentPlayer = (currentPlayer + 1) % numberOfPlayers;
+		}
+
+		private int turn = 1;
+		private int maxTurns = 10;
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var chess = new Chess();
+			chess.Run();
+		}
+	}
+}
