f487ee76c09b9534079ad1485cfda4e5b14bd55a
David Schmoranz <david.schmoranz@outlook.com>
2026-03-23 20:47:59 +0100

    Visitor

---
+++ Visitor_Expression/IExpressionVisitor.cs
@@ -0,0 +1,8 @@
+﻿namespace Visitor_Expression
+{
+    public interface IExpressionVisitor
+    {
+        void Visit(DoubleExpression de);
+        void Visit(AdditionalExpression ae);
+    }
+}
--- Visitor_Expression/Program.cs
+++ Visitor_Expression/Program.cs
@@ -3,54 +3,101 @@
 namespace Visitor_Expression
 {
 
-    using DictType = Dictionary<Type, Action<Expression, StringBuilder>>;
+    //using DictType = Dictionary<Type, Action<Expression, StringBuilder>>;
+
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            var sb = new StringBuilder();
 
             var ex = new AdditionalExpression(
                 new DoubleExpression(1),
                 new AdditionalExpression(new DoubleExpression(2), new DoubleExpression(3)));
 
-            ExpressionPrinter.Print(ex, sb);
-            Console.WriteLine(sb);
+            var exPrinter = new ExpressionPrinter();
+
+            exPrinter.Visit(ex);
+
+
+            var exCalculator = new ExpressionCalculator();
+            exCalculator.Visit(ex);
+            Console.WriteLine(exPrinter.ToString() + "=" + exCalculator.Result);
         }
     }
 
-    public static class ExpressionPrinter
+    public class ExpressionPrinter : IExpressionVisitor
     {
-        private readonly static DictType _actions = new DictType
+        StringBuilder sb = new();
+        public void Visit(DoubleExpression de)
         {
-            [typeof(DoubleExpression)] = (e, sb) =>
-            {
-                var de = (DoubleExpression)e;
-                sb.Append(de.Value);
-            },
-            [typeof(AdditionalExpression)] = (e, sb) => 
-            {
-                var ae = (AdditionalExpression)e;
-                sb.Append('(');
-                Print(ae.Left, sb);
-                sb.Append('+');
-                Print(ae.Right, sb);
-                sb.Append(')');
-            }
-        };
-
-        public static void Print(Expression e, StringBuilder sb)
-        {
-            _actions[e.GetType()](e, sb);
+            sb.Append(de.Value);
         }
 
+        public void Visit(AdditionalExpression ae)
+        {
+            sb.Append('(');
+            ae.Left.Accept(this);
+            sb.Append('+');
+            ae.Right.Accept(this);
+            sb.Append(')');
+        }
+
+        public override string ToString()
+         {
+             return sb.ToString();
+        }
     }
+
+    public class ExpressionCalculator : IExpressionVisitor
+    {
+        public double Result;
+        public void Visit(DoubleExpression de)
+        {
+            Result = de.Value;
+        }
+        public void Visit(AdditionalExpression ae)
+        {
+            ae.Left.Accept(this);
+            var left = Result;
+            ae.Right.Accept(this);
+            var right = Result;
+            Result = left + right;
+        }
+    }
+
+    //public static class ExpressionPrinter
+    //{
+    //    private readonly static DictType _actions = new DictType
+    //    {
+    //        [typeof(DoubleExpression)] = (e, sb) =>
+    //        {
+    //            var de = (DoubleExpression)e;
+    //            sb.Append(de.Value);
+    //        },
+    //        [typeof(AdditionalExpression)] = (e, sb) => 
+    //        {
+    //            var ae = (AdditionalExpression)e;
+    //            sb.Append('(');
+    //            Print(ae.Left, sb);
+    //            sb.Append('+');
+    //            Print(ae.Right, sb);
+    //            sb.Append(')');
+    //        }
+    //    };
+
+    //    public static void Print(Expression e, StringBuilder sb)
+    //    {
+    //        _actions[e.GetType()](e, sb);
+    //    }
+
+    //}
 
 
     public abstract class Expression
     {
         //public abstract void Print(StringBuilder sb);
+        public abstract void Accept(IExpressionVisitor visitor);
     }
 
     public class DoubleExpression : Expression
@@ -60,6 +107,12 @@ namespace Visitor_Expression
         public DoubleExpression(double value)
         {
             this.Value = value;
+        }
+
+        public override void Accept(IExpressionVisitor visitor)
+        {
+            //double dispatch
+            visitor.Visit(this);
         }
     }
 
@@ -73,5 +126,10 @@ namespace Visitor_Expression
             this.Right = right;
         }
 
+        public override void Accept(IExpressionVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
     }
+
 }
